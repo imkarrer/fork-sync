8372286d2d826350d9f28c4411cac5049063ec17
Isaac Karrer <isaac.karrer@elastic.co>
2018-12-13 16:58:59 -0600

    Remove hardcoded artifact

--- updateForkedBranch.sh
+++ updateForkedBranch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ ! -d cloud ]; then
+if [ ! -d ${REPO} ]; then
 	git clone git@github.com:${GH_USER}/${REPO}.git
-	pushd cloud
-	git remote add upstream git@github.com:${UPSTREAM}/cloud.git
+	pushd ${REPO}
+	git remote add upstream git@github.com:${UPSTREAM}/${REPO}.git
 	popd
 fi
 
-pushd cloud && git checkout -f ${BRANCH} && git fetch upstream && git rebase upstream/${BRANCH} && git push && echo "Updated ${BRANCH} " || (echo "Failed to update ${BRANCH}" && exit 1)
+pushd ${REPO} && git checkout -f ${BRANCH} && git fetch upstream && git rebase upstream/${BRANCH} && git push && echo "Updated ${BRANCH} " || (echo "Failed to update ${BRANCH}" && exit 1)
